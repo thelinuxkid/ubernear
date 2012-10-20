716f848755323837016b759ab8919fa3b14a95c7
Andres Buritica <andres@thelinuxkid.com>
2012-10-20 01:11:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
+import os
 
 EXTRAS_REQUIRES = dict(
     web=[
@@ -36,6 +37,12 @@ for k,v in EXTRAS_REQUIRES.iteritems():
         continue
     EXTRAS_REQUIRES['test'] += v
 
+# Pypi package documentation
+root = os.path.dirname(__file__)
+path = os.path.join(root, 'README.rst')
+with open(path) as fp:
+    long_description = fp.read()
+
 setup(
     name='ubernear',
     version='0.0.1',
@@ -45,6 +52,12 @@ setup(
         " RESTful API"
         ),
     packages = find_packages(),
+    author='Andres Buritica',
+    author_email='andres@thelinuxkid.com',
+    maintainer='Andres Buritica',
+    maintainer_email='andres@thelinuxkid.com',
+    url='https://github.com/thelinuxkid/ubernear',
+    license='MIT',
     namespace_packages = ['ubernear'],
     test_suite='nose.collector',
     install_requires=[
